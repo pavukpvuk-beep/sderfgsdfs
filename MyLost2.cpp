3ee85258e970135dc06502eae641670f3426f87f
pavukpvuk-beep <pavukpvuk@gmail.com>
2025-09-26 17:15:04 +0900

    Add files via upload

---
+++ MyLost2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+/*int main() {
+	setlocale(LC_ALL, "ru");
+	int grade1, grade2, grade3, grade4, grade5;
+	double average;
+
+
+	cout << "Введите 5 оценок студента: ";
+	cin >> grade1 >> grade2 >> grade3 >> grade4 >> grade5;
+	average = (grade1 + grade2 + grade3 + grade4 + grade5) / 5.0;
+
+	switch (average >= 4.0) {
+	case true:
+		cout << "Студент допущен до экзамена" << endl;
+		break;
+	case false:
+		cout << "Студент не допущен до экзамена" << endl;
+		break;
+	}
+}*/
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int number;
+	double results;
+}
